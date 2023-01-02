64e8e394022e53d14b184dd1d60a75105651951c
REFAT786 <mahafuzarrahmanrefat786@gmail.com>
2023-01-02 18:09:47 +0600

    delete

--- Final-Assignment/src/main/java/com/exception/BadRequestAlertException.java
+++
@@ -1,8 +0,0 @@
-package com.exception;
-
-public class BadRequestAlertException extends RuntimeException {
-
-    public BadRequestAlertException(String message) {
-        super(message);
-    }
-}
--- Final-Assignment/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello Spring!</h2>
-<%@ include file="WEB-INF/views/admin/employee.jsp"%>
-</body>
-</html>
